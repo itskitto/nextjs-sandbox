d8384a4a527aed9ae78cdbda581ec83e739a9f0b
itskitto <kitto.khr@gmail.com>
2021-06-08 13:09:09 -0400

    add Navigation menu skeleton component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Navigation from "../components/Navigation";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Nextjs Sandbox</title>
         <meta name="description" content="Random Nextjs snippets" />
@@ -11,8 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Nextjs Sandbox</h1>
+        <Navigation />
       </main>
-    </div>
+    </>
   );
 }
